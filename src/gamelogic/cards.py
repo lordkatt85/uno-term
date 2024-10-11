672deb735065948b03654038a0a89f9c28cde3a4
lordkatt85 <Surendhran2003@gmail.com>
2024-10-11 20:44:50 +0530

    update cards.py

--- src/gamelogic/cards.py
+++ src/gamelogic/cards.py
@@ -9,58 +9,36 @@ class Card:
         return f'Card(color={self.color}, number={self.number})'
 
 class Deck:
-    def __init__(self, cards):
-        self.cards = cards        
-        def create_deck(self):
-            colors = ['Red', 'Green', 'Blue', 'Yellow']
-            numbers = list(range(10)) + ['Reverse', 'Skip', 'Take Two']
-        
-            uno_cards = [
-                Card(color, number)
-                for color in colors
-                for number in numbers
-            ] + [
-                Card('Wild', 'Wild') for _ in range(8)
-            ] + [
-                Card(color, 'Reverse') for color in colors
-            ] + [
-                Card(color, 'Skip') for color in colors
-            ] + [
-                Card(color, 'Take Two') for color in colors
-            ] + [
-                Card('Wild', 'Wild Draw Four') for _ in range(4)
-            ]
+    def __init__(self):
+        self.cards = []
+        self.create_deck()
     
-            return uno_cards
+    def create_deck(self):
+        colors = ['Red', 'Green', 'Blue', 'Yellow']
+        numbers = list(range(10)) + ['Reverse', 'Skip', 'Take Two']
+    
+        self.cards = [
+            Card(color, number)
+            for color in colors
+            for number in numbers
+        ] + [
+            Card('Wild', 'Wild') for _ in range(8)
+        ] + [
+            Card(color, 'Reverse') for color in colors
+        ] + [
+            Card(color, 'Skip') for color in colors
+        ] + [
+            Card(color, 'Take Two') for color in colors
+        ] + [
+            Card('Wild', 'Wild Draw Four') for _ in range(4)
+        ]
 
     def shuffle(self):
         random.shuffle(self.cards)
 
     def __repr__(self):
         return f'Deck(cards={self.cards})'
-
-# Create a list of Uno cards
-colors = ['Red', 'Green', 'Blue', 'Yellow']
-numbers = list(range(10)) + ['Reverse', 'Skip', 'Take Two']
-
-uno_cards = [
-    Card(color, number)
-    for color in colors
-    for number in numbers
-] + [
-    Card('Wild', 'Wild') for _ in range(8)
-] + [
-    Card(color, 'Reverse') for color in colors
-] + [
-    Card(color, 'Skip') for color in colors
-] + [
-    Card(color, 'Take Two') for color in colors
-] + [
-    Card('Wild', 'Wild Draw Four') for _ in range(4)
-]
-
-# Create a Deck instance
-deck = Deck(uno_cards)
-
-# Shuffle the Deck
+    
+deck = Deck()
 deck.shuffle()
+print(deck.cards)
