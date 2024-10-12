fcaeccea8810b8425dceab9fc15a42bd418bfd9f
lordkatt85 <Surendhran2003@gmail.com>
2024-10-12 09:03:51 +0530

    add start message in main.py

--- src/main.py
+++ src/main.py
@@ -13,5 +13,21 @@ for i in range(n):
     Player_list[i] = gamelogic.players.Player("suren")
     Player_list[i].draw_initial_cards(gamelogic.cards.deck)
 
+game_state = 0
+
 if __name__ == "__main__":
     print(Player_list[1].name, lister(Player_list[1].hand))
+    print(gamelogic.cards.deck)
+    if game_state == 0:
+        print("Welcome to a FanMade Uno game inside the terminal.")
+        print("The goal of the game is to get rid of all your cards.")
+        print("You can draw cards from the deck, and play them on the board.")
+        print("If you don't have a card you want to play, you can draw a new card from the deck.")
+        print("You can play a card that you have on the board or a card that you have in your hand.")
+        print("You can use any card that you have in your hand or draw a new card from the deck.")
+        print("If you don't have a card you want to play, you can draw a new card from the deck.")
+        input("when all players are connected, press enter")
+        game_state = 1
+
+    while game_state:
+        pass
